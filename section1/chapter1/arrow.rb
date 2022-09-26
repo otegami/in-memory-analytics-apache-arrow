405757724c7fea2fbe20ff48a0e88262d8e603f5
otegami <a.s.takuya1026@gmail.com>
2022-09-27 08:42:11 +0900

    Finish to read Chapter1

--- section1/chapter1/arrow.rb
+++ section1/chapter1/arrow.rb
@@ -24,8 +24,7 @@ columns = [
 pp columns
 
 puts '-- record_batch --'
-binding.irb
-record_batch = Arrow::RecordBatch.new(schema, 4, columns)
+record_batch = Arrow::RecordBatch.new(schema, columns)
 pp record_batch
 
 puts '-- record_batch.schema --'
@@ -33,3 +32,75 @@ pp record_batch.schema
 
 puts '-- record_batch.n_rows --'
 pp record_batch.n_rows
+
+## Record Batch
+# - 均等な長さの Array のグループや Schema を参照する
+# - 大きな Dataset の row のサブセットとして扱われる
+#   - Struct にとても似ている
+#
+# Struct Archer {
+#   archer: string
+#   location: string
+#   year: int16
+# }
+
+# Building a struct array
+data_type = Arrow::StructDataType.new(
+  archer: {type: :string},
+  location: {type: :string},
+  year: {type: :int16}
+)
+values = [
+['archer1', 'here', 2000],
+['archer2', 'there', 2022],
+['archer3', 'over there', nil],
+]
+
+puts '-- Struct Array --'
+struct_array = Arrow::StructArray.new(data_type, values)
+pp struct_array
+
+# Using record batches and zero-copy manipulation
+fields = [
+  Arrow::Field.new(:archer, :string),
+  Arrow::Field.new(:location, :string),
+  Arrow::Field.new(:year, :int16),
+]
+schema = Arrow::Schema.new(fields)
+record_batch = Arrow::RecordBatch.new(schema, struct_array)
+pp record_batch
+
+# record_batch.raw_records
+# -> [["archer1", "here", 2000], ["archer2", "there", 2022]]
+
+pp record_batch.slice(0, 3)
+pp record_batch.slice(1, 1)
+
+# Handling none values
+
+row_data = { id: 4, cost: 241.21, cost_components: [100.00, 140.10, 1.11] }
+
+list_description = {
+  name: "cost_components",
+  type: :list,
+  field: {
+    name: "component",
+    type: :float
+  }
+}
+fields = [
+  Arrow::Field.new("id", :int8),
+  Arrow::Field.new("cost",  :float),
+  Arrow::Field.new(list_description)
+]
+data_type = Arrow::ListDataType.new(name: "component", type: :float)
+
+schema = Arrow::Schema.new(fields)
+columns = [
+  Arrow::Int8Array.new(row_data[:id]),
+  Arrow::FloatArray.new(row_data[:cost]),
+  Arrow::ListArray.new(data_type, [row_data[:cost_components]])
+]
+
+record_batch = Arrow::RecordBatch.new(schema, 1, columns)
+pp record_batch
