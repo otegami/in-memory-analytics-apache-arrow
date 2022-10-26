8674aab8d1f4ab45bd1a7080208cdac53af83747
otegami <a.s.takuya1026@gmail.com>
2022-10-27 08:35:29 +0900

    Reading Creating a sample dataset

--- chapter7/README.md
+++ chapter7/README.md
@@ -1,2 +1,14 @@
 # Chapter7: Using the Arrow Datasets API
 
+- Querying multifile datasets
+- Filtering data programmatically
+- Using the Datasets API in Python
+- Streaming results
+
+## Querying multifile datasets
+
+### Creating a sample dataset
+- Arrow Dataset ができること
+  - ファイルの読み書き
+  - 指定されたディレクトリ下のファイルをクロール
+  - データセットに Schema がない際は Metadata から推測
--- chapter7/arrow.rb
+++ chapter7/arrow.rb
@@ -1,22 +1,36 @@
 require 'arrow'
+require 'parquet'
 
 builder = Arrow::Int64ArrayBuilder.new
-builder.append([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+builder.append_values([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 column1 = builder.finish
 builder.reset
 
-builder.append([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reverse)
+builder.append_values([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reverse)
 column2 = builder.finish
 builder.reset
 
-builder.append([1, 2, 1, 2, 1, 2, 1, 2, 1, 2])
+builder.append_values([1, 2, 1, 2, 1, 2, 1, 2, 1, 2])
 column3 = builder.finish
 builder.reset
 
 fields = [
-  Arrow::Field.new("int64",  :int64),
-  Arrow::Field.new("int64",  :int64),
-  Arrow::Field.new("int64",  :int64),
+  Arrow::Field.new("a",  :int64),
+  Arrow::Field.new("b",  :int64),
+  Arrow::Field.new("c",  :int64),
 ]
 schema = Arrow::Schema.new(fields)
-record_batch = Arrow::RecordBatch.new(schema, [column1, column2, column3])
+p column1
+p n_rows = column1.length
+p record_batch = Arrow::RecordBatch.new(schema, n_rows, [column1, column2, column3])
+# p table = Arrow::Table.new(schema, [column1, column2, column3])
+
+Arrow::FileOutputStream.open("./tmp/file.arrow", false) do |output|
+  Arrow::RecordBatchFileWriter.open(output, schema) do |writer|
+    writer.write_record_batch(record_batch)
+  end
+end
+
+table = Arrow::Table.load('./tmp/file.arrow')
+p table
+p table.slice { |slicer| slicer['a'] < 4 }
